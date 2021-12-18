6abab6f378965771926463a01b457e6485edee6f
WayneCher <aaazwg@msn.cn>
2021-12-18 17:56:59 +0800

    Ex02

---
+++ EX02/exercise 02-1.py
@@ -0,0 +1,51 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import math
+from mpl_toolkits import mplot3d
+import os
+from functions import *
+
+# Read the data from file
+
+dir = os.path.join(os.getcwd(),'ex2data1.txt')
+
+# Use pandas package
+
+df = pd.read_csv(dir, sep = ',', names = ['Test1','Test2','Admin'])
+
+# Scatter plot the data
+
+row_num = df.Area.size
+x1 = df['Area'].values
+x2 = df['Bedroom'].values
+y = df['Price'].values
+
+# iteration
+def iter_fun(alpha = 0.01, count = 0):
+    df_theta = np.zeros(df.shape[1])
+    theta_val = np.zeros(df.shape[1])
+    j_val = []
+    #initialize J function
+
+    f = func(df_theta, df, row_num)
+    for i in range(count):   
+        j_val.append(f.j())
+        for i in range(len(df_theta)):
+            df_theta[i] = df_theta[i] - alpha * f.sum_part(i)
+        theta_val = np.row_stack((theta_val, df_theta))
+        f.set_theta(df_theta)
+    return j_val, theta_val
+
+#main function
+if __name__ == '__main__':
+    val = iter_fun(count = 20000)
+    counts = np.arange(1,20001,1)
+    print(val[1][-1])
+    plot(counts, val[0], 'counts', 'J Value')
+    scatter_plot(x1, x2, y, size = 20, marker = 'x', theta = [22.1,72.4,54.6])
+    print(vali(df))
+    
+    
+
+
---
+++ EX02/functions.py
@@ -0,0 +1,72 @@
+# Some functions used in homework
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+# Scatter plot to validate the correction 
+# size is the plot size of dots, marker is the shape of dots 
+def scatter_plot(x1,x2, y, size = 10, marker = '*', color = 'red', x_label = ' ', y_label = ' ',theta=[]):
+    plt.scatter(x1,y,s = size, marker = marker, c = color)
+    plt.scatter(x2,y,s = size, marker = marker, c = color)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    if theta:
+        plt.plot(x1, theta[0] + theta[1] * x1 + theta[2] * x2)
+        plt.plot(x2, theta[0] + theta[1] * x1 + theta[2] * x2)
+    plt.show()
+
+# linear plot
+def plot(x, y, x_label, y_label):
+    plt.plot(x,y)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.show()
+
+
+# Derivative function
+class func(object):
+
+    def __init__(self, theta, var, row_num = 0):
+        theta = np.array(theta)
+        for index,row in var.iterrows():
+            if index == 0:
+                new_var = np.array(row)
+            else:
+                new_var = np.row_stack((new_var,row))
+        new_var = np.insert(new_var, 0, values = np.ones((1,row_num)), axis = 1)
+        self.theta = theta
+        self.var = new_var
+        self.row_num = row_num
+
+    def set_theta(self, new_theta):
+        self.theta = new_theta
+        
+    def sum_part(self,theta_flag = 0):
+        sum = 0
+        for i in range(self.row_num):
+            sum = sum + (np.sum(np.dot(self.var[i][0:-1], self.theta.T)) - self.var[i][-1]) * self.var[i][theta_flag]
+        return sum / self.row_num
+
+    def j(self):
+        sum = 0
+        for i in range(self.row_num):
+            sum = sum + np.power((np.sum(np.dot(self.var[i][0:-1], self.theta.T)) - self.var[i][-1]),2)
+        return sum / (2 * self.row_num)
+
+def vali(df):
+    for index, row in df.iterrows():
+        if index == 0:
+            x = np.array(row[0:-1])
+            y = np.array(row[-1])
+        else:
+            x = np.row_stack((x,row[0:-1]))
+            y = np.row_stack((y,row[-1]))
+    x = np.insert(x, 0, values = np.ones((1,47)), axis = 1)
+    theta = np.dot((np.dot(np.linalg.inv(np.dot(x.T ,x)), x.T)),y)
+    return theta 
+        
+        
+    
+    
+
+    
